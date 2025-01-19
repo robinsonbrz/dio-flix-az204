07890e6a918723e8fa265768a4f2dec25e2ba0f0
Robinson Enedino <robinsonbrz@gmail.com>
2025-01-19 01:54:03 -0300

    feat: storage e database

---
+++ fnPostDataStorage/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Server.Kestrel;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();    
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.Limits.MaxRequestBodySize = 1024 * 1024 * 10; // 10 MB
+});
+
+builder.Build().Run();
